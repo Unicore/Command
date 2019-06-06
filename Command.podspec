2409af9761506ea08a4d644d046054fb67e6ca11
Maxim Bazarov <bazaroffma@gmail.com>
2019-06-06 10:55:24 +0200

    Adds SPM support (#2)
    * add spm
    * add CocoaPods support

--- Command.podspec
+++ Command.podspec
@@ -1,10 +1,11 @@
 Pod::Spec.new do |s|
 
   s.name = 'Command'
-  s.version = '1.1.0'
+  s.version = '1.0.1'
   s.swift_version = '4.2'
   s.license = 'MIT'
-  s.summary = 'A convenient Swift closures wrapper'
+  s.summary = 'The Command pattern implementation'
+  s.description = 'Applying a pattern Command we have a more readable and protected form side-effects data structure, which gives you a closure without a possibility of returning a result.'
   s.homepage = 'https://github.com/Unicore/Command'
   s.authors = { 'Maxim Bazarov' => 'bazaroffma@gmail.com' }
   s.source = { :git => 'https://github.com/Unicore/Command.git', :tag => s.version }
@@ -13,6 +14,6 @@ Pod::Spec.new do |s|
   s.osx.deployment_target = '10.10'
   s.tvos.deployment_target = '9.0'
   s.watchos.deployment_target = '2.0'
-  s.source_files = 'Sources/**/*.swift'
+  s.source_files = 'Sources/Command/**/*.swift'
 
 end
--- Sources/Command/Command.swift
+++ Sources/Command/Command.swift
@@ -92,17 +92,25 @@ extension CommandOf where T == Void {
     }
 }
 
-/// Allows PlainCommands to be compared and stored in sets and dicts.
-/// Uses `ObjectIdentifier` to distinguish between PlainCommands
+/// Allows Command to be compared and stored in sets and dicts.
+/// Uses `ObjectIdentifier` to distinguish between Commands
 extension CommandOf: Hashable, Equatable {
-    public static
+    public static 
     func ==(left: CommandOf, right: CommandOf) -> Bool {
         return ObjectIdentifier(left) == ObjectIdentifier(right)
     }
-    
-    public
+
+    public 
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self).hashValue)
+    }
+
+    #if !swift(>=5)
+    public 
     var hashValue: Int { return ObjectIdentifier(self).hashValue }
+    #endif
 }
+
 
 // MARK: - Value binding
 public
@@ -111,7 +119,6 @@ extension CommandOf {
     ///
     /// - Parameter value: Value to be bound
     /// - Returns: Command with having `value` when executed
-    public
     func bound(to value: T) -> Command {
         return Command { self.execute(with: value) }
     }
@@ -129,7 +136,6 @@ extension CommandOf {
 public
 extension CommandOf {
     
-    public
     func async(on queue: DispatchQueue) -> CommandOf {
         return CommandOf { value in
             queue.async {
